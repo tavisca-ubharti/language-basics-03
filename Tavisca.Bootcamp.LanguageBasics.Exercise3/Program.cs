208fd364868bd541c7a19d40410c763b633448bd
Ujjwal Bharti <ubharti@tavisca.in>
2019-06-21 17:58:57 +0530

    Added implementation of SelectMeals

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
@@ -40,8 +41,153 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-            // Add your code here.
+            var calorie = new int[protein.Length];
+            var result = new List<int>();
+            for(int i=0;i<protein.Length;i++)
+            {
+                calorie[i] = (protein[i] + carbs[i])*4 + fat[i]*9;
+            }
+            var index = new List<int>();
+            foreach (var item in dietPlans)
+            {
+                index.Clear();
+               if(string.IsNullOrWhiteSpace(item))
+                {
+                    index.Clear();
+                    index.Add(0);
+                }
+               else
+                {
+                    foreach (var ch in item)//CcPpFfTt
+                    {
+                        if (index.Count == 1)
+                            break;
+                        if (ch == 'C')
+                        {
+                            FindMaxIndex(carbs, index);
+                        }
+                        else if (ch == 'c')
+                        {
+                            FindMinIndex(carbs, index);
+                        }
+                        else if (ch == 'P')
+                        {
+                            FindMaxIndex(protein, index);
+                        }
+                        else if (ch == 'p')
+                        {
+                            FindMinIndex(protein, index);
+                        }
+                        else if (ch == 'F')
+                        {
+                            FindMaxIndex(fat, index);
+                        }
+                        else if (ch == 'f')
+                        {
+                            FindMinIndex(fat, index);
+                        }
+                        else if (ch == 'T')
+                        {
+                            FindMaxIndex(calorie, index);
+                        }
+                        else if (ch == 't')
+                        {
+                            FindMinIndex(calorie, index);
+                        }
+                    }
+                }
+                result.Add(index[0]);
+            }
+            int[] diet = new int[dietPlans.Length];
+            result.CopyTo(diet);
+            result.Clear();
+            return diet;
             throw new NotImplementedException();
+        }
+
+        private static void FindMinIndex(int[] arr, List<int> index)
+        {
+            var list = new List<int>();
+            var minValue = int.MaxValue;
+            if(index.Count==0)
+            {
+                list.Clear();
+                for(int i=0;i<arr.Length;i++)
+                {
+                    if(minValue==arr[i])
+                    {
+                        list.Add(i);
+                    }
+                    if(minValue>arr[i])
+                    {
+                        list.Clear();
+                        minValue = arr[i];
+                        list.Add(i);
+                    }
+                }
+            }
+            else
+            {
+                list.Clear();
+                foreach(var i in index)
+                {
+                    if (minValue == arr[i])
+                    {
+                        list.Add(i);
+                    }
+                    if (minValue > arr[i])
+                    {
+                        list.Clear();
+                        minValue = arr[i];
+                        list.Add(i);
+                    }
+                }
+            }
+            index.Clear();
+            for (int i = 0; i < list.Count; i++)
+                index.Add(list[i]);
+        }
+        private static void FindMaxIndex(int[] arr, List<int> index)
+        {
+            var list = new List<int>();
+            var maxValue = int.MinValue;
+            if (index.Count == 0)
+            {
+                list.Clear();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (maxValue == arr[i])
+                    {
+                        list.Add(i);
+                    }
+                    if (maxValue < arr[i])
+                    {
+                        list.Clear();
+                        maxValue = arr[i];
+                        list.Add(i);
+                    }
+                }
+            }
+            else
+            {
+                list.Clear();
+                foreach (var i in index)
+                {
+                    if (maxValue == arr[i])
+                    {
+                        list.Add(i);
+                    }
+                    if (maxValue < arr[i])
+                    {
+                        list.Clear();
+                        maxValue = arr[i];
+                        list.Add(i);
+                    }
+                }
+            }
+            index.Clear();
+            for (int i = 0; i < list.Count; i++)
+                index.Add(list[i]);
         }
     }
 }
